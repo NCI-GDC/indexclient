18bc70945b326ae7c6a59bd48ff50bd36f09a10a
mattreex <mattreex.9@gail.com>
2021-07-14 15:48:58 -0500

    add test

--- tests/test_client.py
+++ tests/test_client.py
@@ -275,8 +275,9 @@ def test_query_urls__include(indexd_loader, indexd_client, include, expectation)
 
 @pytest.mark.parametrize("params, expected", [
     ({"url":"s3://localhost:7000/test_bucket_2", "key":"type", "value":"aws", "page_size":2}, 4),
-    ({"url":"s3://localhost:7000/test_bucket", "key":"state", "value":"validated", "page_size":3}, 5),
+    ({"url":"s3://localhost:7000/test_bucket", "key":"type", "value":"cleversafe", "page_size":3}, 5),
     ({"url":"", "key":"type", "value":"invalidated", "page_size":2}, 0)
+    ({"url":"s3://localhost:7000/test_bucket/03.txt", "key":"type", "value":"validated", "page_size":3}, 1)
 ])
 def test_query_urls_metadata(indexd_loader, indexd_client, params, expected):
     indexd_loader("tests/data/documents.json")
