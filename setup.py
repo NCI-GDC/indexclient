42fd4330565f11c3d9184647bc6dc009442d9c6d
mattreex <mattreex.9@gail.com>
2021-08-06 14:23:50 -0500

    use find packages

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='indexclient',
     version='2.0.0',
-    packages=[
-        'indexclient',
-        'indexclient.parsers',
-        'indexd_test_utils'
-    ],
+    packages=find_packages(),
     install_requires=[
         'requests~=2.5',
     ],
