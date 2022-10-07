7f8348f2fcd03370e5a62a65727cb391f26dd1a2
Qiao Qiao <qiaoq@uchicago.edu>
2022-10-07 10:10:04 -0500

    get mock server host from env

--- indexd_test_utils2/__init__.py
+++ indexd_test_utils2/__init__.py
@@ -6,6 +6,7 @@ import pytest
 import requests
 import flask
 import socket
+import os
 
 from pytest_postgresql.janitor import DatabaseJanitor
 
@@ -144,7 +145,8 @@ def create_indexd_tables(index_driver, alias_driver, auth_driver):
 
 @pytest.fixture
 def create_indexd_tables_no_migrate(
-        index_driver_no_migrate, alias_driver_no_migrate, auth_driver):
+        index_driver_no_migrate, alias_driver_no_migrate, auth_driver
+):
     """Make sure the tables are created but don't operate on them directly.
 
     There is no migration required for the SQLAlchemyAuthDriver.
@@ -172,37 +174,38 @@ def indexd_server(pg_url):
     settings = indexd_settings.get_settings(pg_url)
     app_init(app, settings)
 
-    hostname = 'localhost'
+    host = os.getenv("INDEXD_HOST") or 'localhost'
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     debug = False
 
     port = random.randint(8000, 9000)
-    while sock.connect_ex((hostname, port)) == 0:
+    while sock.connect_ex((host, port)) == 0:
         port = random.randint(8000, 9000)
 
-    t = threading.Thread(target=app.run, kwargs={'host': hostname, 'port': port, 'debug': debug})
+    t = threading.Thread(target=app.run, kwargs={'host': host, 'port': port, 'debug': debug})
     t.setDaemon(True)
     t.start()
 
-    wait_for_indexd_alive(port)
-    yield MockServer(port=port)
+    wait_for_indexd_alive(host, port)
+    yield MockServer(host=host, port=port)
 
 
-def wait_for_indexd_alive(port):
-    url = f'http://localhost:{port}'
+def wait_for_indexd_alive(host, port):
+    url = f'http://{host}:{port}'
     try:
         requests.get(url)
     except requests.ConnectionError:
-        return wait_for_indexd_alive(port)
+        return wait_for_indexd_alive(host, port)
     else:
         return
 
 
 class MockServer:
-    def __init__(self, port):
+    def __init__(self, host, port):
+        self.host = host
         self.port = port
-        self.baseurl = f'http://localhost:{port}'
+        self.baseurl = f'http://{host}:{port}'
 
 
 def create_random_index(index_client, did=None, version=None, hashes=None):
