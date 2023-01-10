412686dea7984c4820e408a25918fe33f079dcc8
Qiao Qiao <qiaoq@uchicago.edu>
2023-01-10 13:19:14 -0600

    add indexd_loader to plugin

--- pytest_indexd/plugin.py
+++ pytest_indexd/plugin.py
@@ -7,5 +7,5 @@ from pytest_indexd.db import (
     pg_url,
     postgresql_server_indexd,
 )
-from pytest_indexd.indexd_client_fixture import indexd_client
+from pytest_indexd.indexd_client_fixture import indexd_client, indexd_loader
 from pytest_indexd.indexd_server_fixture import indexd_server
