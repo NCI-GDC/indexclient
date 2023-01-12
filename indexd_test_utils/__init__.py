60fee945df80c7d0dedf0ed0d2fdf23c71d71115
Qiao Qiao <qiaoq@uchicago.edu>
2023-01-11 18:42:35 -0600

    set host to PG_HOST to try_drop_test_data

--- indexd_test_utils/__init__.py
+++ indexd_test_utils/__init__.py
@@ -33,8 +33,9 @@ def setup_indexd_test_database(request):
 
     # try_drop_test_data() is run before the tests starts and after the tests
     # complete. This ensures a clean database on start and end of the tests.
-    setup_database(host=PG_HOST)
-    request.addfinalizer(try_drop_test_data)
+    yield setup_database(host=PG_HOST)
+
+    try_drop_test_data(host=PG_HOST)
 
 
 def truncate_tables(driver, base):
