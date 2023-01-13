462b6030d0c0130ff07e6a2b50e9b514a5f365e5
Qiao Qiao <qiaoq@uchicago.edu>
2023-01-13 10:39:52 -0600

    update pin for indexd

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
             "pytest~=6.2.0",
             "sqlalchemy~=1.3.0",
             "sqlalchemy-utils>=0.32,<0.36.4",
-            "indexd @ git+https://github.com/NCI-GDC/indexd.git@5932bddbc27048855e55735e1522f08cadd50222#egg=indexd",
+            "indexd @ git+https://github.com/NCI-GDC/indexd.git@ce1cdd71430c2ec985d8c32750890c747e961ade#egg=indexd",
         ],
         "pytest_indexd": [
             "deepdiff",
@@ -26,7 +26,7 @@ setup(
             "pytest-parallel",
             "pytest-postgresql",
             "pytest-xdist",
-            "indexd @ git+https://github.com/NCI-GDC/indexd.git@5932bddbc27048855e55735e1522f08cadd50222#egg=indexd",
+            "indexd @ git+https://github.com/NCI-GDC/indexd.git@ce1cdd71430c2ec985d8c32750890c747e961ade#egg=indexd",
             "indexdmodels @  git+ssh://git@github.com/NCI-GDC/indexdmodels.git@53da40cfd4040f7bb5abc837d93d4664aa8394e1#egg=indexdmodels",
         ],
     },
