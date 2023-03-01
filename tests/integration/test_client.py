45ee16913a72c1a9f8368aeb6d0948b5f88528f4
mpsolano <mikesolano@uchicago.edu>
2023-03-01 17:39:21 -0600

    DEV-1026: Add exclude_deleted params and test cases

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -105,11 +105,15 @@ def test_get_latest_version_with_skip(index_client):
     assert v_doc.baseid == doc_2.baseid
 
 
-def test_get_latest_version_with_skip_deleted(index_client):
-    """
-    Tests retrieval of latest record version not flagged as deleted
+def test_get_latest_version_with_exclude_deleted(index_client):
+    """Test retrieval of latest record version using exclude_deleted param.
+
+    This test creates two documents, a primary document with default values
+    and then a deleted version of this document. It then verifies that using
+    the exclude_deleted parameter gets the correct version of the document.
+
     Args:
-        index_client (indexclient.client.IndexClient): IndexClient Pytest Fixture
+        index_client (indexclient.client.IndexClient): IndexClient fixture
     """
     non_deleted_doc = create_random_index(index_client)
     did = non_deleted_doc.did
@@ -118,10 +122,8 @@ def test_get_latest_version_with_skip_deleted(index_client):
     deleted_doc.metadata = {"deleted": "True"}
     deleted_doc.patch()
 
-    assert non_deleted_doc == index_client.get_latest_version(did)
-    assert deleted_doc == index_client.get_latest_version(
-        did, skip_deleted_versions=False
-    )
+    assert non_deleted_doc == index_client.get_latest_version(did, exclude_deleted=True)
+    assert deleted_doc == index_client.get_latest_version(did)
 
 
 @pytest.mark.parametrize("arg, exception", [("AAA", HTTPError), (None, TypeError)])
@@ -170,11 +172,15 @@ def test_list_versions(index_client):
     assert len(versions) == 2
 
 
-def test_list_versions_with_delete(index_client):
-    """
-    Tests retrieval of all versions of document not flagged as deleted
+def test_list_versions_with_exclude_deleted(index_client):
+    """Test retrieval of all versions of a doc using exclude_deleted param.
+
+    This test creates a version hierarchy of documents with some marked as
+    deleted. It then verifies that using the exclude_deleted parameter filters
+    deleted documents from the results.
+
     Args:
-        index_client (indexclient.client.IndexClient): IndexClient Pytest Fixture
+        index_client (indexclient.client.IndexClient): IndexClient fixture
     """
     doc = create_random_index(index_client)
     did = doc.did
@@ -192,12 +198,11 @@ def test_list_versions_with_delete(index_client):
             doc.patch()
             deleted_docs.append(doc)
 
-    assert set(index_client.list_versions(did, skip_deleted_versions=False)) == set(
-        non_deleted_docs + deleted_docs
-    ), "the versions returned do not match all records created"
-    assert set(index_client.list_versions(did)) == set(
-        non_deleted_docs
-    ), "the versions returned do not match non-deleted records created"
+    includes_deleted = index_client.list_versions(did)
+    assert set(includes_deleted) == set(non_deleted_docs + deleted_docs)
+
+    excludes_deleted = index_client.list_versions(did, exclude_deleted=True)
+    assert set(excludes_deleted) == set(non_deleted_docs)
 
 
 def test_updating_metadata(index_client):
@@ -305,29 +310,41 @@ def test_bulk_get_latest_with_skip_null(index_client):
     assert {doc.did for doc in docs_null_included} == null_dids
 
 
-def test_bulk_get_latest_with_skip_deleted(index_client):
-    """Test bulk_get_latest() with skip_deleted parameters"""
+def test_bulk_get_latest_with_exclude_deleted(index_client):
+    """Test bulk retrieval of latest versions of docs using exclude_deleted.
+
+    This test creates several version hierarchies of documents with the latest
+    versions in each hierarchy marked as deleted. It then verifies that using
+    the exclude_deleted parameter returns the correct latest version of docs
+    in each hierarchy.
+
+    Args:
+        index_client (indexclient.client.IndexClient): IndexClient fixture
+    """
     dids = []
-    new_dids = set()
-    deleted_dids = set()
+    latest_non_deleted_dids = set()
+    latest_dids = set()
+
     for i in range(20):
         doc = create_random_index(index_client)
         rev_doc = create_random_index_version(index_client, did=doc.did)
+        latest_non_deleted_dids.add(rev_doc.did)
+
         if i < 5:
-            deleted_doc = create_random_index_version(index_client, did=doc.did)
-            deleted_doc.metadata = {"deleted": "True"}
-            deleted_doc.patch()
+            latest_doc = create_random_index_version(index_client, did=doc.did)
+            latest_doc.metadata = {"deleted": "True"}
+            latest_doc.patch()
         else:
-            deleted_doc = rev_doc
+            latest_doc = rev_doc
 
         dids.append(doc.did)
-        new_dids.add(rev_doc.did)
-        deleted_dids.add(deleted_doc.did)
+        latest_dids.add(latest_doc.did)
 
-    docs_deleted_excluded = index_client.bulk_get_latest(dids, skip_deleted=True)
-    docs_deleted_included = index_client.bulk_get_latest(dids, skip_deleted=False)
-    assert {doc.did for doc in docs_deleted_excluded} == new_dids
-    assert {doc.did for doc in docs_deleted_included} == deleted_dids
+    includes_deleted = index_client.bulk_get_latest(dids)
+    assert set(doc.did for doc in includes_deleted) == latest_dids
+
+    excludes_deleted = index_client.bulk_get_latest(dids, exclude_deleted=True)
+    assert set(doc.did for doc in excludes_deleted) == latest_non_deleted_dids
 
 
 @pytest.mark.parametrize(
@@ -368,6 +385,36 @@ def test_query_urls__include(indexd_loader, indexd_client, include, expectation)
         assert any([include in url for url in entry["urls"]])
         total_urls += 1
     assert expectation == total_urls
+
+
+def test_query_url_exclude_deleted(index_client):
+    """Test doc retrieval using the query_url exclude_deleted param.
+
+    This test creates several documents, marking some documents as deleted.
+    It then verifies that using the exclude_deleted parameter filters out the
+    deleted documents.
+
+    Args:
+        index_client (indexclient.client.IndexClient): IndexClient fixture
+    """
+    non_deleted_count = 0
+    deleted_count = 0
+
+    for i in range(10):
+        doc = create_random_index(index_client)
+
+        if i % 2 == 0:
+            non_deleted_count += 1
+        else:
+            doc.metadata = {"deleted": "True"}
+            doc.patch()
+            deleted_count += 1
+
+    includes_deleted = list(index_client.query_url())
+    assert len(includes_deleted) == non_deleted_count + deleted_count
+
+    excludes_deleted = list(index_client.query_url(exclude_deleted=True))
+    assert len(excludes_deleted) == non_deleted_count
 
 
 @pytest.mark.parametrize(
@@ -430,3 +477,44 @@ def test_query_urls_metadata(indexd_loader, indexd_client, params, expected):
     urls = indexd_client.query_urls_metadata(**params)
 
     assert expected == len(list(urls))
+
+
+def test_query_urls_metadata_exclude_deleted(index_client):
+    """Test doc retrieval using the query_urls_metadata exclude_deleted param.
+
+    This test creates several documents, marking some documents as deleted.
+    It then verifies that using the exclude_deleted parameter filters out the
+    deleted documents.
+
+    Args:
+        index_client (indexclient.client.IndexClient): IndexClient fixture
+    """
+    # all docs created with create_random_index() have a url matching this pattern and a corresponding key-value pair
+    url_match = "s3://ceph.service.consul/"
+    key = "type"
+    value = "cleversafe"
+
+    non_deleted_count = 0
+    deleted_count = 0
+
+    for i in range(10):
+        doc = create_random_index(index_client)
+
+        if i % 2 == 0:
+            non_deleted_count += 1
+        else:
+            doc.metadata = {"deleted": "True"}
+            doc.patch()
+            deleted_count += 1
+
+    includes_deleted = list(
+        index_client.query_urls_metadata(url=url_match, key=key, value=value)
+    )
+    assert len(includes_deleted) == non_deleted_count + deleted_count
+
+    excludes_deleted = list(
+        index_client.query_urls_metadata(
+            url=url_match, key=key, value=value, exclude_deleted=True
+        )
+    )
+    assert len(excludes_deleted) == deleted_count
