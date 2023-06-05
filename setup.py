2212b57d503bb42d4aed780dfeb30d1b97659ce8
Qiao Qiao <qiaoq@uchicago.edu>
2023-06-05 16:49:46 -0500

    remove restrain for importlib-resources

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "pytest_indexd": [
             "deepdiff<5.8",  # for python3.6
             "dataclasses; python_version < '3.7'",
-            "importlib_resources; python_version < '3.7'",
+            "importlib_resources",
             "psycopg",
             "pytest<7",
             "pytest-cov[toml]",
