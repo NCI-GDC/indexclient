a23f76a0de271a3559ae420b0dbbf80a7b92a0e9
ProfOak <OpenProfOak@gmail.com>
2023-09-18 15:43:19 -0500

    DEV-2133: Add readme to long_description
    The release step of the gitlab pipeline complains about this missing.

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="indexclient",
     setup_requires=["setuptools_scm<7"],
+    long_description=long_description,
     use_scm_version={
         "local_scheme": "no-local-version",
         "write_to": "indexclient/_version.py",
