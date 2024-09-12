93ce2089f8c87751879edc2bd94bc490cce58dad
Michael Solano <96085518+mpsolano@users.noreply.github.com>
2024-09-12 12:14:48 -0500

    DEV-2993: Handle hashlib.md5 for python 3.9+ (#52)

--- src/pytest_indexd/utils.py
+++ src/pytest_indexd/utils.py
@@ -81,7 +81,9 @@ def create_random_index_version(
     Returns:
         Document: the document just created
     """
-    md5_hasher = hashlib.md5()
+    md5_hasher = (
+        hashlib.md5() if sys.version_info < (3, 9) else hashlib.md5(usedforsecurity=False)
+    )  # nosec
     md5_hasher.update(did.encode("utf-8"))
     file_name = did
 
